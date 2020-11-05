7a16a9e8ad3b5d574b9caa6fa981457e18396ea7
hrs2203 <hrishabh2203@gmail.com>
2020-11-05 17:23:38 +0530

    djongo_installed

--- kalakriti/kalakriti/settings.py
+++ kalakriti/kalakriti/settings.py
@@ -73,12 +73,21 @@ WSGI_APPLICATION = 'kalakriti.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'localhost',
     }
 }
+
+
 
 
 # Password validation
